1661baefcc48cff8816220afd835e63b7ba1e77c
emiiblesstm-star <emiiblesstm@gmail.com>
2025-08-26 11:45:24 +0300

    Update main.tsx

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -45,6 +45,8 @@ const Dtrader = lazy(() => import('../dtrader'));
 const FreeBots = lazy(() => import('../free-bots/free-bots.tsx')); // Assuming you created free-bots.tsx
 const Analysis = lazy(() => import('../analysis/analysis'));
 const AiPage = lazy(() => import('../ai/ai')); // Assuming you created AiPage.tsx
+const Tool = lazy(() => import('../tool/tool'));
+const SignalPage = lazy(() => import('../signal/signal')); // Assuming you created SignalPage.tsx
 
 const AnalysisToolIcon = () => (
     <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -63,6 +65,15 @@ const AiPageIcon = () => (
 </svg>
 );
 
+const ToolIcon = () => (
+   <svg fill="var(--text-general)" width="20px" height="20px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" data-name="Layer 1"><path d="M10,13H4a1,1,0,0,0-1,1v6a1,1,0,0,0,1,1h6a1,1,0,0,0,1-1V14A1,1,0,0,0,10,13ZM9,19H5V15H9ZM20,3H14a1,1,0,0,0-1,1v6a1,1,0,0,0,1,1h6a1,1,0,0,0,1-1V4A1,1,0,0,0,20,3ZM19,9H15V5h4Zm1,7H18V14a1,1,0,0,0-2,0v2H14a1,1,0,0,0,0,2h2v2a1,1,0,0,0,2,0V18h2a1,1,0,0,0,0-2ZM10,3H4A1,1,0,0,0,3,4v6a1,1,0,0,0,1,1h6a1,1,0,0,0,1-1V4A1,1,0,0,0,10,3ZM9,9H5V5H9Z"/></svg>
+);
+
+const SignalPageIcon = () => (
+    <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M8 6.00067L21 6.00139M8 12.0007L21 12.0015M8 18.0007L21 18.0015M3.5 6H3.51M3.5 12H3.51M3.5 18H3.51M4 6C4 6.27614 3.77614 6.5 3.5 6.5C3.22386 6.5 3 6.27614 3 6C3 5.72386 3.22386 5.5 3.5 5.5C3.77614 5.5 4 5.72386 4 6ZM4 12C4 12.2761 3.77614 12.5 3.5 12.5C3.22386 12.5 3 12.2761 3 12C3 11.7239 3.22386 11.5 3.5 11.5C3.77614 11.5 4 11.7239 4 12ZM4 18C4 18.2761 3.77614 18.5 3.5 18.5C3.22386 18.5 3 18.2761 3 18C3 17.7239 3.22386 17.5 3.5 17.5C3.77614 17.5 4 17.7239 4 18Z" stroke="var(--text-general)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+);
 
 const AppWrapper = observer(() => {
     const { connectionStatus } = useApiBase();
@@ -447,6 +458,22 @@ const AppWrapper = observer(() => {
                         >
                             <Suspense fallback={<ChunkLoader message={localize('Please wait, loading page...')} />}>
                                 <AiPage />
+                            </Suspense>
+                           </div>
+                        <div label={<><ToolIcon /><Localize i18n_default_text='Pro Tool' /></>} id='id-tool'
+                             onClick={() => handleLinkChange('AiPage')}
+                            style={{ cursor: 'pointer' }}
+                        >
+                            <Suspense fallback={<ChunkLoader message={localize('Please wait, loading page...')} />}>
+                                <Tool />
+                            </Suspense>
+                           </div>
+                        <div label={<><SignalPageIcon /><Localize i18n_default_text='Exp signal' /></>} id='id-signal'
+                             onClick={() => handleLinkChange('signal')}
+                            style={{ cursor: 'pointer' }}
+                        >
+                            <Suspense fallback={<ChunkLoader message={localize('Please wait, loading page...')} />}>
+                                <SignalPage />
                             </Suspense>
                            </div>
                         </Tabs>
